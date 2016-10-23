739ec5f710541be98d786940faf7aaa153b8e7ba
Ashley Hindle <hey@ashleyhindle.com>
2016-10-23 11:16:08 +0100

    Initial setup of Reader/Writer and composer

--- Reader.php
+++ Reader.php
@@ -8,119 +8,32 @@ use TodoMove\Intercessor\Tag;
 use TodoMove\Intercessor\Tags;
 use TodoMove\Intercessor\Task;
 
-class Reader extends TodoMove\Intercessor\Service\Reader
+class Reader extends \TodoMove\Intercessor\Service\AbstractReader
 {
-    private $xml        = []; // Array from (array) simplexml_load_*
-    private $of         = []; // OmniFocus store for XML versions of tasks/tags/projects/folders/etc
-    private $tags       = []; // key = tagid
-    private $folders    = []; // key = folderid
-    private $projects   = []; // key = projectid
-    private $tasks      = []; // key = taskid (you can get a group of tasks from projects, so no need to group them by projectid
-
     /**
-     * OmnifocusReader constructor.
+     * Reader constructor.
      *
-     * @param array $xml - This should be the result of a simplexml_load_* call
+     * Modify the constructor to accept whatever it is you need to be able to parse.
+     *
+     * For OmniFocus this is XML, for Wunderlist it might be a configured Wunderlist API class
+     *
      */
     public function __construct($xml)
     {
-        $this->xml = is_array($xml) ? $xml : (array) $xml;
+        $this->name('ServiceName');
 
-        $this->parseContexts(); // Must be first as Projects/Tasks utilise them
+        $this->parseTags(); // Must be first as Projects/Tasks utilise them
         $this->parseFolders(); // Folders before projects, as we attach the projects to the folders
         $this->parseProjects(); // This attaches projects to folders
         $this->parseTasks(); // This attaches tasks to projects
-	$this->name('Reader');
-    }
-
-    /**
-     * @return Tag[]
-     */
-    public function tags()
-    {
-        return $this->tags;
-    }
-
-    /**
-     * @param $contextId - contextId from OmniFocus's XML
-     *
-     * @return Tag
-     */
-    public function tag($contextId)
-    {
-        return $this->tags[$contextId];
-    }
-
-    /**
-     * @return Task[]
-     */
-    public function tasks()
-    {
-        return $this->tasks;
-    }
-
-    /**
-     * @param $taskId - taskId from OmniFocus's XML
-     *
-     * @return Task
-     */
-    public function task($taskId)
-    {
-        return $this->tasks[$taskId];
-    }
-
-    /**
-     * @return ProjectFolder[]
-     */
-    public function folders()
-    {
-        return $this->folders;
-    }
-
-    /**
-     * @param $folderId
-     *
-     * @return ProjectFolder
-     */
-    public function folder($folderId)
-    {
-        return $this->folders[$folderId];
-    }
-
-    /**
-     * @return Project[]
-     */
-    public function projects()
-    {
-        return $this->projects;
-    }
-
-    /**
-     * @param $projectId
-     *
-     * @return Project
-     */
-    public function project($projectId)
-    {
-        return $this->projects[$projectId];
     }
 
     /**
      * @return $this
      */
-    public function parseContexts()
+    public function parseTags()
     {
-        $this->of['contexts'] = [];
-        $this->tags = [];
-
-        foreach ($this->xml['context'] as $context) {
-            $contextId = (string)$context->attributes()['id'];
-            $this->of['contexts'][$contextId] = $context;
-
-            $tag = new Tag((string) $context->name);
-            $this->tags[$contextId] = $tag;
-        }
-
+        // TODO: Add \TodoMove\Intercessor\Tag's to the tags array, keyed by id: $this->addTag($tag);
         return $this;
     }
 
@@ -129,37 +42,7 @@ class Reader extends TodoMove\Intercessor\Service\Reader
      */
     protected function parseProjects()
     {
-        $this->of['contexts'] = [];
-        $this->projects = [];
-
-        // Get tasks, that are actually projects, that are _not_ completed
-        $xmlProjectsFiltered = array_filter($this->xml['task'], function($var) {
-            return (! empty((array)$var->project) && empty((string)$var->completed));
-        });
-
-        foreach ($xmlProjectsFiltered as $xmlProject) {
-            $projectId = (string)$xmlProject->attributes()['id'];
-            $folderId = (string)$xmlProject->project->folder->attributes()['idref'];
-
-            $xmlProjects[$projectId] = $xmlProject;
-            $status = (string)$xmlProject->project->status;
-            $status = $status ?: 'active';
-            $projectTags = new Tags();
-
-            if ($xmlProject->context) {
-                $contextId = (string) $xmlProject->context->attributes()['idref'];
-                if (!empty($contextId)) {
-                    $projectTags->add($this->tag($contextId));
-                }
-            }
-
-            $project = new Project( (string) $xmlProject->name);
-            $project->status($status);
-            $project->tags($projectTags);
-
-            $this->projects[$projectId] = $project;
-            $this->folder($folderId)->project($project);
-        }
+        // TODO: Add \TodoMove\Intercessor\Project's to the projects array, keyed by id: $this->addProject($project);
 
         return $this;
     }
@@ -169,33 +52,7 @@ class Reader extends TodoMove\Intercessor\Service\Reader
      */
     public function parseFolders()
     {
-        $this->of['folders'] = [];
-        $this->folders = [];
-
-        foreach ($this->xml['folder'] as $xmlFolder) {
-            if ((bool)$xmlFolder->hidden === true) {
-                //continue;
-            }
-
-            $folder = new ProjectFolder((string) $xmlFolder->name);
-            $folderId = (string)$xmlFolder->attributes()['id'];
-            $this->folders[$folderId] = $folder;
-        }
-
-        foreach ($this->xml['folder'] as $xmlFolder) {
-            if ((bool)$xmlFolder->hidden === true) {
-                //continue;
-            }
-
-            if (array_key_exists('folder', $xmlFolder)) { // It is a child and has a parent
-                $parentFolderId = (string) $xmlFolder->folder->attributes()['idref'];
-
-                $folder = $this->folders[(string)$xmlFolder->attributes()['id']];
-                $folder->parent($this->folders[$parentFolderId]);
-
-                $this->folders[$parentFolderId]->child($folder);
-            }
-        }
+        // TODO: Add \TodoMove\Intercessor\ProjectFolder's to the projects array, keyed by id: $this->addFolder($folder);
 
         return $this;
     }
@@ -205,192 +62,8 @@ class Reader extends TodoMove\Intercessor\Service\Reader
      */
     public function parseTasks()
     {
-        $this->of['tasks'] = [];
-        $this->tasks = [];
-
-        // Only return tasks that aren't projects, and aren't completed
-        $xmlTasksFiltered = array_filter($this->xml['task'], function($var) {
-            return (empty((array)$var->project) && empty((string)$var->completed));
-        });
-
-        foreach ($xmlTasksFiltered as $xmlTask) {
-            $taskId = (string) $xmlTask->attributes()['id'];
-            $projectId = (string) $xmlTask->task->attributes()['idref'];
-
-            $taskTags = new Tags();
-
-            if (property_exists($xmlTask, 'context')) {
-                $contextId = (string)$xmlTask->context->attributes()['idref'];
-                if (!empty($contextId)) {
-                    $taskTags->add($this->tag($contextId));
-                }
-            }
-
-            $task = (new Task((string)$xmlTask->name))
-                ->created(new \DateTime((string)$xmlTask->added))
-                ->flagged((bool) $xmlTask->flagged)
-                ->tags($taskTags);
-
-            if (!empty((string) $xmlTask->start)) {
-                $task->defer(new \DateTime((string) $xmlTask->start));
-            }
-
-            if (!empty((string) $xmlTask->due)) {
-                $task->due(new \DateTime((string) $xmlTask->due));
-            }
-
-            if (!empty($xmlTask->note)) {
-                $task->notes(trim($this->buildNotes((array) $xmlTask->note)));
-            }
-
-            if (!empty((string)$xmlTask->{'repetition-rule'})) {
-                $repetition = (string) $xmlTask->{'repetition-rule'};
-                $rule = new \Recurr\Rule($repetition, new \DateTime(null, new \DateTimeZone('UTC')), null, 'UTC');
-                $rule->setCount(2);
-
-                preg_match('/FREQ=(?<freq>[A-Z]+)/', $repetition, $matches);
-                $freq = strtoupper($matches['freq']);
-
-                $repeat = new Repeat();
-                $repeat->interval($rule->getInterval());
-
-                // We only support basic options for the minute (not specific week days for example)
-                switch ($freq) {
-                    case 'DAILY':
-                        $type = Repeat::DAY;
-                        break;
-                    case 'WEEKLY':
-                        $type = Repeat::WEEK;
-                        break;
-                    case 'MONTHLY':
-                        $type = Repeat::MONTH;
-                        break;
-                    case 'YEARLY':
-                        $type = Repeat::YEAR;
-                        break;
-                    default:
-                        throw new \Exception('Repetition-rule frequency not supported: ' . $freq);
-                }
-
-                $repeat->type($type);
-
-                $task->repeat($repeat);
-            }
-
-            if (!empty($projectId)) {
-                $task->project($this->project($projectId));
-                $this->project($projectId)->task($task);
-            }
-
-            $this->tasks[$taskId] = $task;
-        }
+        // TODO: Add \TodoMove\Intercessor\Project's to the projects array, keyed by id: $this->addTask($task);
 
         return $this;
-    }
-
-    /**
-     * @param string $notes - notes from an OmniFocus task
-     * @return string
-     */
-    private function buildNotes($notes)
-    {
-        $note = '';
-
-        if (is_string($notes)) {
-            return $notes . PHP_EOL;
-        }
-
-        if (is_array($notes) || $notes instanceof SimpleXMLElement) {
-            foreach ((array) $notes as $item) {
-                $note .= $this->buildNotes($item);
-            }
-        }
-
-        return $note;
-    }
-
-    /**
-     * Pass in the path to a file from an OmniFocus backup (extracted from the zip) - it's likely called contents.xml
-     * @param string $filename
-     * @throws InvalidArgumentException
-     *
-     * @return OmnifocusReader
-     */
-    public static function loadXML($filename = 'contents.xml')
-    {
-        if (empty($filename)) {
-            Throw new InvalidArgumentException('Filename is empty');
-        }
-
-        if (!is_readable($filename)) {
-            Throw new InvalidArgumentException('File is not readable: ' . $filename);
-        }
-
-        $xml = simplexml_load_file($filename);
-        if ($xml === false) {
-            Throw new InvalidArgumentException('File is not XML: ' . $filename);
-        }
-
-        return new static((array)$xml);
-    }
-
-    /**
-     * @param string $string - Valid XML, likely from contents.xml of an Omnifocus backup
-     *
-     * @return OmnifocusReader
-     */
-    public static function loadString($string = '')
-    {
-        $xml = simplexml_load_string($string);
-        if ($xml === false) {
-            Throw new InvalidArgumentException('String is not valid XML: ' . $string);
-        }
-
-        return new static((array)$xml);
-    }
-
-    /**
-     * OmniFocus backup from File->Export->File Format->Backup Documents (Omnifocus).
-     * ~/Downloads/OmniFocus.ofocus-backup/00000000000000=fQ_pq1_r7Jz+xxxxx-xxxxx.zip
-     *
-     * @param string $filename
-     * @throws Exception
-     *
-     * @returns OmnifocusBackupReader
-     */
-    public static function loadBackup($filename = 'OmniFocus.zip')
-    {
-        if (empty($filename)) {
-            Throw new InvalidArgumentException('Filename is empty');
-        }
-
-        if (!is_readable($filename)) {
-            Throw new InvalidArgumentException('File is not readable: ' . $filename);
-        }
-
-        if (! class_exists('ZipArchive')) {
-            Throw new Exception('ZipArchive is not installed');
-        }
-
-        $zip = new \ZipArchive;
-        if ($zip->open($filename) === TRUE) {
-            $xmlString = $zip->getFromName('contents.xml');
-            if (empty($xmlString)) {
-                Throw new LogicException('This isn\'t a valid OmniFocus backup .zip file.  It is missing contents.xml');
-            }
-            $zip->close();
-        } else {
-            Throw new Exception('Failed to open ZipArchive: ' . $filename);
-        }
-
-        return static::loadString($xmlString);
-    }
-
-    /**
-     * @return array
-     */
-    public function xml()
-    {
-        return $this->xml;
     }
 }
--- Writer.php
+++ Writer.php
@@ -2,7 +2,70 @@
 
 namespace TodoMove\Service\ServiceName;
 
-class Writer extends TodoMove\Intercessor\Service\AbstractWriter
-{
+use TodoMove\Intercessor\Contracts\Service\Reader;
+use TodoMove\Intercessor\Folder;
+use TodoMove\Intercessor\Project;
+use TodoMove\Intercessor\Service\AbstractWriter;
+use TodoMove\Intercessor\Tag;
+use TodoMove\Intercessor\Task;
 
+class Writer extends AbstractWriter
+{
+    public function _construct()
+    {
+        $this->name('ServiceName');
+    }
+
+    // TODO: How will we handle live updates of progress?  We'll need to mark each item as 'synced', then laravel echo can be used to say what's been synced?
+    // TODO: Maybe we need an event/callback: $project->onSync(function($project) { echo::default('project.synced', $project); });
+
+    /** @inheritdoc */
+    public function syncFrom(Reader $reader)
+    {
+        $this->syncTags($reader->tags());
+        $this->syncFolders($reader->folders());
+        $this->syncProjects($reader->projects());
+        $this->syncTasks($reader->tasks());
+    }
+
+    public function syncFolder(Folder $folder)
+    {
+        // TODO: Implement syncFolder() method.
+    }
+
+    public function syncProject(Project $project)
+    {
+        // TODO: Implement syncProject() method.
+    }
+
+    public function syncTask(Task $task)
+    {
+        // TODO: Implement syncTask() method.
+    }
+
+    public function syncTag(Tag $tag)
+    {
+        // TODO: Implement syncTag() method.
+    }
+
+
+    protected function syncFolders(array $folders)
+    {
+        //TODO: Loop, and use $this->syncFolder(Folder $folder) to hit appropriate API's to add folders / throw exceptions.  Handling errors will be tough?
+    }
+
+    protected function syncProjects(array $projects)
+    {
+        //TODO: Loop, and use $this->syncProject(Project $project) to hit appropriate API's to add folders / throw exceptions.  Handling errors will be tough?
+    }
+
+    protected function syncTags(array $tags)
+    {
+        //TODO: Loop, and use $this->syncTag(Tag $tag) to hit appropriate API's to add folders / throw exceptions.  Handling errors will be tough?
+    }
+
+    protected function syncTasks(array $tasks)
+    {
+        //TODO: Loop, and use $this->syncTask(Task $task) to hit appropriate API's to add folders / throw exceptions.  Handling errors will be tough?
+    }
 }
